845492618c2becf2e73934bf3fa167ae46d80b0d
“Munkh-Erdene” <munkh.hustle@gmail.com>
2024-09-23 21:58:04 +0900

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import re
 
 # Define the folder where the mp4 files are located
-folder_path = r"C:\Users\thinkpad\Downloads\5D"
+folder_path = r"C:\Users\thinkpad\Downloads\10D"
 
 # Loop through all files in the folder
 for filename in os.listdir(folder_path):
