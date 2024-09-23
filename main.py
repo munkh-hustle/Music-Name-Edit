4d2849d6eef14cd1d7b7d4c814d3a351d247ea3b
“Munkh-Erdene” <munkh.hustle@gmail.com>
2024-09-24 02:05:05 +0900

    ...

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 import re
 
-# Define the folder where the mp4 files are located
+# Define the folder where the mp3 files are located
 folder_path = r"C:\Users\thinkpad\Downloads\10D"
 
 # Loop through all files in the folder
 for filename in os.listdir(folder_path):
     print(f"Processing: {filename}")  # Print each file being processed
 
-    # Check if the file is an mp4 file and contains the pattern
+    # Check if the file is an mp3 file and contains the pattern
     if filename.endswith(".mp3") and "[SPOTIFY-DOWNLOADER.COM]" in filename:
         print(f"Match found: {filename}")  # Print when a match is found
         
